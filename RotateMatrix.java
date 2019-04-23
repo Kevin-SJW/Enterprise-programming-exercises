938dcbc425cdf3dab58af5f2eec6e2dce903b71f
Kevin <shijianweicisco@163.com>
2019-04-23 13:09:13 +0800

    RotateMatrix.java

---
+++ RotateMatrix.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+/**
+ * @Classname RotateMatrix
+ * @Description TODO
+ * ou are given an n x n 2D matrix representing an image.
+ * Rotate the image by 90 degrees (clockwise).
+ * Note:
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ * Example 1:
+ * Given input matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ * @Date 2019/4/23 12:00
+ * @Created by 14241
+ */
+public class RotateMatrix {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[][] matrix=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+//        System.out.println(matrix);
+        roateMatrix(matrix);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+"\t");
+            }
+            System.out.println("\r\n");
+        }
+
+    }
+    public static void roateMatrix(int[][] matrix){
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
+            }
+        }
+
+    }
+}
