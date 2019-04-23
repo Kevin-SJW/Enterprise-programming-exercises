777be1f8313548de0549b73e9d2292fc489da726
Kevin <shijianweicisco@163.com>
2019-04-23 13:10:05 +0800

    RotateMatrix.java update

--- RotateMatrix.java
+++ RotateMatrix.java
@@ -38,7 +38,7 @@ public class RotateMatrix {
         roateMatrix(matrix);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                System.out.print(matrix[i][j]+"\t");
+                System.out.print(matrix[i][j]+" ");
             }
             System.out.println("\r\n");
         }
